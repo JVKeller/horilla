b735082947d18d7c83617819dd99c247eded3311
JVKeller <keller.jeff@gmail.com>
2024-12-09 15:21:22 -0500

    add time server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+# Update package list
+echo "Updating package list..."
+apt update
+
+# Install NTP
+echo "Installing NTP..."
+apt install -y ntp
+apt install -y nano
+
+# Set the timezone to New York
+echo "Setting timezone to New York..."
+timedatectl set-timezone America/New_York
+
+# Restart NTP service to apply changes
+echo "Restarting NTP service..."
+systemctl restart ntp
+
+echo "NTP installation and timezone configuration completed."
 
 echo "Waiting for database to be ready..."
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
